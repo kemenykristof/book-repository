b6fed77e859c521b58ee282ea2c5dc6de0479a04
kemenykristof <kemeny.kristof24@gmail.com>
2019-11-25 14:45:45 +0100

    major component refactor and firebase progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import BookContextProvider from "./contexts/BookContext";
-
 import ThemeContextProvider from "./contexts/ThemeContext";
-
+import { AuthProvider } from "./contexts/AuthContext";
 import AppContainer from "./components/AppContainer";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./registration/Home";
+import Login from "./registration/Login";
+import SignUp from "./registration/SignUp";
 
 function App() {
   return (
@@ -13,6 +16,15 @@ function App() {
           <AppContainer></AppContainer>
         </ThemeContextProvider>
       </BookContextProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <div>
+            <Route exact path="/home" component={Home} />
+            <Route exact path="login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/firebase/Firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyAp2TP6rjCjpwvqCYb6h6B7eE9oDAw0K8E",
+  authDomain: "book-repository-8c4a4.firebaseapp.com",
+  databaseURL: "https://book-repository-8c4a4.firebaseio.com",
+  projectId: "book-repository-8c4a4",
+  storageBucket: "book-repository-8c4a4.appspot.com",
+  messagingSenderId: "1025700069993",
+  appId: "1:1025700069993:web:fd887e872f718d0e2118f8",
+  measurementId: "G-RVQ0WG412B"
+});
+
+export default app;
