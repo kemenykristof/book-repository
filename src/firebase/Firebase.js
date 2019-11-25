95c75aa175858587fa0a83933ed7a8bde73fbaf5
kemenykristof <kemeny.kristof24@gmail.com>
2019-11-25 15:40:21 +0100

    firestore revert fix

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -2,14 +2,14 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAp2TP6rjCjpwvqCYb6h6B7eE9oDAw0K8E",
+  authDomain: "book-repository-8c4a4.firebaseapp.com",
+  databaseURL: "https://book-repository-8c4a4.firebaseio.com",
+  projectId: "book-repository-8c4a4",
+  storageBucket: "book-repository-8c4a4.appspot.com",
+  messagingSenderId: "1025700069993",
+  appId: "1:1025700069993:web:fd887e872f718d0e2118f8",
+  measurementId: "G-RVQ0WG412B"
 });
 
 export default app;
