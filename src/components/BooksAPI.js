196354a3276a49ee32e217086bb014a4161d493d
kemenykristof <kemeny.kristof24@gmail.com>
2019-11-25 10:16:28 +0100

    api refactor

--- src/components/BooksAPI.js
+++ src/components/BooksAPI.js
@@ -2,41 +2,64 @@ import React, { useState, Component } from "react";
 import SearchBox from "./SearchBox";
 import request from "superagent";
 import BookListAPI from "./BookListAPI";
+import axios from "axios";
 
-class BooksAPI extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: [],
-      searchField: ""
-    };
-  }
+const BooksAPI = () => {
+  const [books, setBooks] = useState({ items: [] });
+  const [searchTerm, setSearchTerm] = useState("");
+  const onInputChange = e => {
+    setSearchTerm(e.target.value);
+  };
 
-  searchBook = e => {
+  let API_URL = `https://www.googleapis.com/books/v1/volumes`;
+
+  const fetchBooks = async () => {
+    const result = await axios.get(`${API_URL}?q=${searchTerm}`);
+    setBooks(result.data);
+    console.log(result.data);
+  };
+
+  const onSubmitHandler = e => {
     e.preventDefault();
-    request
-      .get("https://www.googleapis.com/books/v1/volumes")
-      .query({ q: this.state.searchField })
-      .then(data => {
-        this.setState({ books: [...data.body.items] });
-      });
+    fetchBooks();
   };
 
-  handleSearch = event => {
-    console.log(event.target.value);
-    this.setState({ searchField: event.target.value });
-  };
-  render() {
-    return (
-      <div>
-        <SearchBox
-          searchBook={this.searchBook}
-          handleSearch={this.handleSearch}
-        ></SearchBox>
-        <BookListAPI books={this.state.books}></BookListAPI>
-      </div>
-    );
-  }
-}
+  return (
+    <section>
+      <form onSubmit={onSubmitHandler}>
+        <label>
+          <span>Search for books</span>
+          <input
+            type="search"
+            placeholder="microservice, restful design, etc.,"
+            value={searchTerm}
+            onChange={onInputChange}
+          />
+
+          <button type="submit">Search</button>
+        </label>
+      </form>
+      <ul>
+        {books.items.map((book, index) => {
+          return (
+            <li key={index}>
+              <div>
+                <img
+                  alt={`${book.volumeInfo.title} book`}
+                  src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
+                />
+                <div>
+                  <h3>{book.volumeInfo.title}</h3>
+                  <p>{book.volumeInfo.publishedDate}</p>
+                </div>
+              </div>
+              <hr />
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
 
 export default BooksAPI;
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -4,16 +4,6 @@ import uuid from "uuid/v1";
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  /*  const starterData = [
-    { title: "Clean Code", author: "Robert C. Martin", id: uuid() },
-    {
-      title: "Cracking the Coding Interview",
-      author: "Gayle Laakmann McDowell",
-      id: uuid()
-    },
-    { title: "Mastery", author: "Robert Greene", id: uuid() }
-  ]
-  */
 
   const [books, setBooks] = useState(() => {
     const localData = localStorage.getItem("books");
