79d464f9fae6cf83d209538fee0dee07550c5e8c
kemenykristof <kemeny.kristof24@gmail.com>
2019-11-26 09:42:16 +0100

    firebase config refactor

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,7 +1,8 @@
-import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
-const app = firebase.initializeApp({
+var config = {
   apiKey: "AIzaSyAp2TP6rjCjpwvqCYb6h6B7eE9oDAw0K8E",
   authDomain: "book-repository-8c4a4.firebaseapp.com",
   databaseURL: "https://book-repository-8c4a4.firebaseio.com",
@@ -10,6 +11,9 @@ const app = firebase.initializeApp({
   messagingSenderId: "1025700069993",
   appId: "1:1025700069993:web:fd887e872f718d0e2118f8",
   measurementId: "G-RVQ0WG412B"
-});
+};
 
-export default app;
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
