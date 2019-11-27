15586afa095ded4c54273daa1a4514dc84b20d64
kemenykristof <kemeny.kristof24@gmail.com>
2019-11-27 11:56:00 +0100

    styled sign up page

--- src/registration/SignUp.jsx
+++ src/registration/SignUp.jsx
@@ -2,6 +2,13 @@ import React, { useCallback } from "react";
 import { withRouter } from "react-router";
 import app from "../firebase/Firebase";
 import { Link } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 const SignUp = ({ history }) => {
   const handleSignUp = useCallback(
@@ -20,26 +27,57 @@ const SignUp = ({ history }) => {
     [history]
   );
 
+  const paperStyle = {
+    marginTop: "8px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  };
+
   return (
-    <div>
-      <h1>Sign up</h1>
-      <form onSubmit={handleSignUp}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Sign Up</button>
-      </form>
-      <div>
-        <Link to="/login">
-          Already have an account registered? Log in here!
-        </Link>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div style={paperStyle}>
+        <Avatar style={{ color: "blue" }} className="avatar">
+          <AccountCircleIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form onSubmit={handleSignUp}>
+          <label>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              name="email"
+              type="email"
+              placeholder="Email"
+              required
+              fullWidth
+            />
+          </label>
+          <label>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              name="password"
+              type="password"
+              placeholder="Password"
+              required
+              fullWidth
+            />
+          </label>
+          <Button fullWidth variant="contained" color="primary" type="submit">
+            Sign Up
+          </Button>
+        </form>
+        <div>
+          <Link to="/login">
+            Already have an account registered? Log in here!
+          </Link>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
