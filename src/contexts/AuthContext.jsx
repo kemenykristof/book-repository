203b103337854816b168323544df518c29566f39
kemenykristof <kemeny.kristof24@gmail.com>
2019-11-27 16:26:03 +0100

    local storage for auth currentuser

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -4,11 +4,18 @@ import app from "../firebase/Firebase";
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(() => {
+    const localData = localStorage.getItem("currentUser");
+    return localData ? JSON.parse(localData) : null;
+  });
 
   useEffect(() => {
     app.auth().onAuthStateChanged(setCurrentUser);
   }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
+  }, [currentUser]);
 
   return (
     <AuthContext.Provider
